c392bebdd44a49dbe03f5136a274a7808053da1e
Niroo Arjuna <niroo.arjuna@gmail.com>
2026-05-15 15:45:42 +1000

    chore:  drop theme switching from Cmd+K palette

--- src/components/kbar/index.tsx
+++ src/components/kbar/index.tsx
@@ -4,7 +4,6 @@ import { KBarAnimator, KBarPortal, KBarPositioner, KBarProvider, KBarSearch } fr
 import { useRouter } from 'next/navigation';
 import { useMemo } from 'react';
 import RenderResults from './render-result';
-import useThemeSwitching from './use-theme-switching';
 import { useFilteredNavGroups } from '@/hooks/use-nav';
 
 export default function KBar({ children }: { children: React.ReactNode }) {
@@ -59,8 +58,6 @@ export default function KBar({ children }: { children: React.ReactNode }) {
   );
 }
 const KBarComponent = ({ children }: { children: React.ReactNode }) => {
-  useThemeSwitching();
-
   return (
     <>
       <KBarPortal>
